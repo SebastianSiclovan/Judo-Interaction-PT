daaaf89890ad9c3000ed8658a9d941f4464c1d57
siclo <siclovansebastian@yahoo.com>
2023-04-15 13:26:06 +0300

    Judo information feature

---
+++ app/src/main/java/com/example/judointeractionpt/JudoHistory.java
@@ -0,0 +1,14 @@
+package com.example.judointeractionpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JudoHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_judo_history);
+    }
+}
